34cb0754294bc1de5d00f26d8f91520bbca20baa
ArtofGaming <turner4965@gmail.com>
2024-08-25 21:06:21 -0700

    Added Expected Platformer Features
    - fixed jump bug
    - changed to MoveAndSlide for better floor detection
    - added coyote time
    - added acceleration and deceleration (values still need tweaking)

--- Scripts/Movement.cs
+++ Scripts/Movement.cs
@@ -6,9 +6,13 @@ public partial class Movement : CharacterBody2D
     [Signal]
     public delegate void HitEventHandler();
 
-    Vector2 velocity = Vector2.Zero;
-    float Speed = 50;
-    private const float Gravity = 200.0f;
+    
+    float Speed = 1000;
+    private const float Gravity = 500.0f;
+    private const float jumpHeight = 6050.0f;
+    public Vector2 velocity = Vector2.Zero;
+    public float targetXVelocity = 1000f;
+    public bool onFloor = false;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -17,27 +21,69 @@ public partial class Movement : CharacterBody2D
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
+    public override void _PhysicsProcess(double delta)
     {
-
-        var velocity = Vector2.Zero; // The player's movement vector.
+        
+        if (!IsOnFloor())
+        {
+            velocity.Y += Vector2.Down.Y * Gravity * (float)delta;
+            //Add timer
+            var TimeSinceWalkedOff = Time.GetUnixTimeFromSystem();
+            if (Time.GetUnixTimeFromSystem() - TimeSinceWalkedOff < 2)
+            {
+                onFloor = true;
+            }
+            else
+            {
+                onFloor = false;
+            }
+        }
+        
+         
+        // The player's movement vector.
 
         if (Input.IsActionPressed("move_right"))
         {
-            velocity.X += 300;
+           if (velocity.X < targetXVelocity)
+            {
+                velocity.X += 300;
+                Speed += 100;
+            }
+            else
+            {
+                velocity.X = 1000;
+            }
+            
         }
 
         if (Input.IsActionPressed("move_left"))
         {
-            velocity.X -= 300;
+            if (velocity.X > targetXVelocity)
+            {
+                velocity.X -= 300;
+                Speed += 100;
+            }
+            else
+            {
+                velocity.X = -1000;
+            }
         }
-
-        if (velocity.Length() > 0)
+        if (!Input.IsAnythingPressed())
         {
-            velocity = velocity.Normalized() * Speed;
+            velocity = velocity.MoveToward(new Vector2(0, 0), -62);
         }
 
-        var collision = MoveAndCollide(velocity * (float)delta);
+        velocity = velocity.Normalized();
+
+
+        Velocity = velocity * (float)delta * Speed;
+        MoveAndSlide();
+        //Causes possiblity of infinte jump
+        //Could turn this bug into a feature and have jump timing
+        if (Input.IsActionPressed("jump") && (IsOnFloor() || onFloor))
+        {
+            velocity.Y = -jumpHeight *Speed;
+        }
 
     }
 }
