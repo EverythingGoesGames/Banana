6dde24315fc229bb2b3a35707f506a658098f3b6
iambrianbui <iambrianbui@yahoo.com>
2024-09-06 01:51:40 -0700

    jump/gravity changes
    added short hop
    decreased velocity at the jump's apex
    increased falling velocity the longer the player falls for

--- Scripts/Movement.cs
+++ Scripts/Movement.cs
@@ -6,22 +6,29 @@ public partial class Movement : CharacterBody2D
 	[Export]
 	float Speed = 50;
 	[Export]
-	private const float Gravity = 950.0f;
+	private const float Gravity = 980.0f;
 	[Export]
-	private const float jumpHeight = 800.0f;
+	private const float jumpHeight = 500.0f;
+	[Export]
+	private const float fallSpeedModifier = 100.0f;
+	[Export]
+	private const float shortHopModifier = 0.8f;
+	
+
 	public Vector2 velocity = Vector2.Zero;
-	public Vector2 targetVelocity = new Vector2(1000,0);
+	public Vector2 targetVelocity = new Vector2(500,0);
 	public bool onFloor = false;
 	private double TimeSinceWalkedOff = 0;
+	
 	private float timeHeld = 0.0f;
-	private float maxHoldTime = 0.1f;
-
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
+	private float shortHopTime = 0.15f;
+	private float maxHoldTime = 0.3f;
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
 	{
-        var TimeSinceWalkedOff = Time.GetUnixTimeFromSystem();
-        //Console.WriteLine("here");
-    }
+		var TimeSinceWalkedOff = Time.GetUnixTimeFromSystem();
+	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
@@ -48,17 +55,17 @@ public partial class Movement : CharacterBody2D
 
 		if (Input.IsActionPressed("move_right"))
 		{
-            if (velocity.X < 0)
-            {
-                velocity.X = -velocity.X/2;
-            }
+			if (velocity.X < 0)
+			{
+				velocity.X = -velocity.X/2;
+			}
 			else
 			{
-                velocity.X += velocity.MoveToward(targetVelocity, 10).X * Speed;
-                Speed += 100 * (float)delta;
-            }
-            
-        }
+				velocity.X += velocity.MoveToward(targetVelocity, 10).X * Speed;
+				Speed += 100 * (float)delta;
+			}
+			
+		}
 
 		if (Input.IsActionPressed("move_left"))
 		{
@@ -68,49 +75,77 @@ public partial class Movement : CharacterBody2D
 			}
 			else
 			{
-                velocity.X += velocity.MoveToward(-targetVelocity, 10).X * Speed;
-                Speed += 100 * (float)delta;
-            }
-            
+				velocity.X += velocity.MoveToward(-targetVelocity, 10).X * Speed;
+				Speed += 100 * (float)delta;
+			}
+			
 		}
 		if (!Input.IsAnythingPressed())
 		{ 
 			velocity = velocity.MoveToward(new Vector2(0, Gravity), 50) *Speed;
 			Speed = 5;
 		}
+		
+		velocity = velocity.Normalized();
 
-     
-
-        velocity = velocity.Normalized();
-
-        if (Input.IsActionJustPressed("jump") && (IsOnFloor() || onFloor) && timeHeld < maxHoldTime)
-        {
-			timeHeld += (float)delta;
-            velocity.Y = -jumpHeight;
-        }
-        else if (!Input.IsActionPressed("jump") && (!IsOnFloor() && !onFloor))
-        {
-			velocity.X /= 2;
-            velocity.Y = Gravity * (float)delta;
-			timeHeld = 0.0f;
-        }
-		else if (Input.IsActionPressed("jump"))
+		//if (Input.IsActionJustPressed("jump") && (IsOnFloor() || onFloor) && timeHeld < maxHoldTime)
+		////  if (Input.IsActionJustPressed("jump")  && timeHeld < maxHoldTime)
+		//{
+			//timeHeld += (float)delta;
+			//if (timeHeld < shortHopTime)
+			//{
+				//velocity.Y = -jumpHeight * shortHopModifier;
+				//GD.Print("shorthop", timeHeld);
+			//}
+			//else
+			//{
+				//velocity.Y = -jumpHeight * 100;
+				//GD.Print("longhop", timeHeld);
+			//}
+//
+		//}
+		//else if (!Input.IsActionPressed("jump") && (!IsOnFloor() && !onFloor))
+		//{
+			//velocity.X /= 2;
+			//velocity.Y = Gravity * (float)delta;
+			//timeHeld = 0.0f;
+		//}
+		//else 
+		
+		//  START BRIAN JUMP/FALL STUFF
+		//  as we fall, we should fall faster 9.8m/s^2 and all that
+		float fallingMultiplier = (float)((Time.GetUnixTimeFromSystem() - TimeSinceWalkedOff) * (Time.GetUnixTimeFromSystem() - TimeSinceWalkedOff));
+		fallingMultiplier = fallingMultiplier * fallSpeedModifier;
+		if (fallingMultiplier > 100) fallingMultiplier = 100;
+		
+		if (Input.IsActionPressed("jump") && onFloor)
 		{
 			timeHeld += (float)delta;
-			GD.Print(timeHeld);
-			if (timeHeld >= maxHoldTime)
+			if (timeHeld < shortHopTime)
 			{
-				velocity.Y = Gravity * (float)delta;
-            }
+				velocity.Y = -jumpHeight * shortHopModifier;
+			}
+			else if (timeHeld > shortHopTime && timeHeld < maxHoldTime)
+			{
+				//  velocity should decrease as we approach the apex of the jump
+				float taper = (1 - timeHeld * 2);
+				velocity.Y = -jumpHeight * shortHopModifier * taper;
+			}
+			else
+			{
+				velocity.Y = Gravity * (float)delta * fallingMultiplier;
+			}
 		}
 		else
 		{
-			velocity.Y = Gravity * (float)delta;
+			velocity.Y = Gravity * (float)delta * fallingMultiplier;
 			timeHeld = 0.0f;
 		}
+		
+		//  END BRIAN JUMP/FALL STUFF
 
 		velocity.X *= Speed;
-        Velocity = velocity;
+		Velocity = velocity;
 		MoveAndSlide();
 		//Causes possiblity of infinte jump
 		//Could turn this bug into a feature and have jump timing
